832114f601c82704103243274e5be6084cb04140
Александр Белёв <belyov97@gmail.com>
2017-12-18 14:00:04 +0500

    task3 complete

---
+++ PayToExit/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dijkstra;
+using GraphLibrary;
+
+namespace PayToExit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new RoomParser("in.txt");
+            var graph = parser.ParseGraph();
+            var dijkstra = graph.FindDijkstraPath(graph.Start, graph.Finish);
+            var result = PrepareOutput(dijkstra, parser.CostLimit, parser.EdgeNumbers);
+            File.WriteAllText("out.txt", result);
+        }
+
+        static string PrepareOutput((List<Node>, int) dijkstraResult, int costLimit, Dictionary<Edge, int> edgeNumbers)
+        {
+            var (path, cost) = dijkstraResult;
+            var result = new List<string>();
+            var doors = new List<string>();
+            for (var i = 0; i < path.Count - 1; i++)
+            {
+                var i1 = i;
+                doors.Add(edgeNumbers[
+                    path[i].Edges.First(edge => edge.AnotherNode(path[i1]).Equals(path[i1 + 1]))].ToString());
+            }
+            if (cost > costLimit)
+            {
+                result.Add("N");
+            }
+            else
+            {
+                result.Add("Y");
+                result.Add(cost.ToString());
+                result.Add(string.Join(" ", doors));
+            }
+            return string.Join("\n", result);
+        }
+    }
+}
---
+++ PayToExit/RoomParser.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GraphLibrary;
+
+namespace PayToExit
+{
+    public class RoomParser : GraphParser
+    {
+        public int CostLimit { get; set; }
+        public Dictionary<Edge, int> EdgeNumbers { get; }
+        public RoomParser(string fileName) : base(fileName)
+        {
+            EdgeNumbers= new Dictionary<Edge, int>();
+        }
+
+        public override Graph ParseGraph()
+        {
+            var lines = File.ReadAllLines("in.txt").Select(line => line.Split().Select(int.Parse).ToArray()).ToArray();
+            var counts = lines[0];
+            var n = counts[0];
+            var m = counts[1];
+            var start = counts[2];
+            var sum = counts[3];
+            CostLimit = sum;
+            var graph = new Graph(n + 1);
+            var costs = lines.Skip(n + 1).SelectMany(x => x).ToArray();
+            var addedRooms = new HashSet<int>();
+            for (var roomNumber = 1; roomNumber <= n; roomNumber++)
+            {
+                var numbers = lines[roomNumber];
+                for (var doorIndex = 1; doorIndex < numbers.Length; doorIndex++)
+                {
+                    if (addedRooms.Contains(numbers[doorIndex]))
+                        continue;
+                    var added = false;
+                    for (var secondRoom = roomNumber + 1; secondRoom <= n; secondRoom++)
+                    {
+                        if (lines[secondRoom].Skip(1).Contains(numbers[doorIndex]))
+                        {
+                            var edge=graph.Connect(graph[roomNumber], graph[secondRoom], costs[numbers[doorIndex] - 1]);
+                            addedRooms.Add(numbers[doorIndex]);
+                            EdgeNumbers[edge] = numbers[doorIndex];
+                            added = true;
+                        }
+                    }
+                    if (!added)
+                    {
+                        var edge=  graph.Connect(graph[roomNumber], graph[n + 1], costs[numbers[doorIndex] - 1]);
+                        EdgeNumbers[edge] = numbers[doorIndex];
+                    }
+                }
+            }
+            //foreach (var exit in lines[n].Where(room => !addedRooms.Contains(room)))
+            //{
+            //    graph.Connect(graph[n], graph[n + 1], costs[exit - 1]);
+            //}
+            
+            graph.Start = graph[start];
+            graph.Finish = graph[n + 1];
+            return graph;
+        }
+    }
+}
